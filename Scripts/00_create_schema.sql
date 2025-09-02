9525d8c6201a81ac2151ff25b19e81e438ebecda
Shyamgupta12 <147966889+Shyamgupta12@users.noreply.github.com>
2025-09-02 20:23:35 +0530

    Add files via upload

---
+++ Scripts/00_create_schema.sql
@@ -0,0 +1,7 @@
+-- =========================
+-- SCHEMA CREATION
+-- Purpose: Define the bronze, silver, and gold schemas for the data warehouse layers
+-- =========================
+CREATE SCHEMA IF NOT EXISTS bronze;
+CREATE SCHEMA IF NOT EXISTS silver;
+CREATE SCHEMA IF NOT EXISTS gold;
